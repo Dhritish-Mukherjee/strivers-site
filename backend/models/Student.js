3a8c8410a2e42acea189177febefe60a87ee64d8
Dhritish Mukherjee <coderdhritish18@gmail.com>
2026-08-31 07:36:22 +0530

    fix: account creation bug with mongoose hook

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -42,11 +42,10 @@ const studentSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-studentSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+studentSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Compare password method
